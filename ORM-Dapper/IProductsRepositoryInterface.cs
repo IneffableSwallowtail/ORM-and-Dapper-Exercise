ec47a015098895ec808bc05c0df126035b4a5d2e
A.J. Miller <ajmiller1206@gmail.com>
2022-10-11 11:45:01 -0400

    filled out the skeleton of Exercise 2

---
+++ ORM-Dapper/IProductsRepositoryInterface.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ORM_Dapper
+{
+    public interface IProductsRepositoryInterface
+    {
+        IEnumerable<Product> GetAllProducts()
+        {
+            throw new NotImplementedException();
+        }
+        void CreateProduct(string name, double price, int categoryID)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
