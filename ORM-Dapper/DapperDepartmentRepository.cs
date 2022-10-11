a0480203dad884052a87ed19497ed5c8f337fdc4
A.J. Miller <ajmiller1206@gmail.com>
2022-10-11 11:13:04 -0400

    filled out the dapper department repository

--- ORM-Dapper/DapperDepartmentRepository.cs
+++ ORM-Dapper/DapperDepartmentRepository.cs
@@ -1,12 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.Common;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Dapper;
 
 namespace ORM_Dapper
 {
-    internal class DapperDepartmentRepository
+    public class DapperDepartmentRepository
     {
+        private readonly IDbConnection _connection;
+        //Constructor
+        public DapperDepartmentRepository(IDbConnection connection)
+        {
+            _connection = connection;
+        }
+        public IEnumerable<Department> GetAllDepartments()
+        {
+            return _connection.Query<Department>("SELECT * FROM Departments;");
+        }
+        public void InsertDepartment(string newDepartmentName)
+        {
+            _connection.Execute("INSERT INTO DEPARTMENTS (Name) VALUES (@departmentName);",
+             new { departmentName = newDepartmentName });
+        }
     }
 }
